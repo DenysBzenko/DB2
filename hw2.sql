a776ebcf5f589f043c253988f9ea40cfa30dfb4f
denysbzenko <bzenko8940@gmail.com>
2024-10-23 18:57:42 +0300

    Add sql code

--- hw2.sql
+++ hw2.sql
@@ -1,5 +1,6 @@
-CREATE DATABASE IF NOT EXISTS opt_db;
+create database opt_db;
 USE opt_db;
+
 
 CREATE TABLE IF NOT EXISTS opt_clients (
     id CHAR(36) PRIMARY KEY,
@@ -25,4 +26,75 @@ CREATE TABLE IF NOT EXISTS opt_orders (
     product_id INT,
     FOREIGN KEY (client_id) REFERENCES opt_clients(id),
     FOREIGN KEY (product_id) REFERENCES opt_products(product_id)
+
 );
+
+
+DROP INDEX idx_clients_status ON opt_clients;
+DROP INDEX idx_products_category ON opt_products;
+DROP INDEX idx_orders_order_date ON opt_orders;
+SHOW INDEX FROM opt_clients;
+
+
+EXPLAIN select c.name, c.surname, p.product_name, o.order_date
+        from opt_orders o
+        join opt_clients c ON o.client_id = c.id
+        join opt_products p ON o.product_id = p.product_id
+        where c.status = 'active'
+        and p.product_category != 'Category2' and p.product_category != 'Category3'
+        and p.product_category != 'Category4' and p.product_category != 'Category5'
+        AND o.order_date between '2024-01-01' and '2024-12-31';
+
+
+
+
+
+
+
+
+
+create index idx_clients_status ON opt_clients (status);
+create index idx_products_category ON opt_products (product_category);
+create index  idx_orders_order_date ON opt_orders (order_date);
+
+
+EXPLAIN with ActiveClients AS (
+    select id, name, surname
+    from opt_clients
+    where status = 'active'
+),
+Category1Products AS (
+    select product_id, product_name
+    from opt_products
+    where product_category = 'Category1'
+),
+FilteredOrders AS (
+    select order_id, client_id, product_id, order_date
+    from opt_orders
+    where order_date between '2024-01-01' and '2024-12-31'
+)
+select ActiveClients.name, ActiveClients.surname, Category1Products.product_name, FilteredOrders.order_date
+    from FilteredOrders
+    join ActiveClients  on FilteredOrders.client_id = ActiveClients.id
+    join Category1Products  on FilteredOrders.product_id = Category1Products.product_id;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
